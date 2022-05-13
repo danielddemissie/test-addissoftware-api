1ff436d7e9fc7edde26b47ebab32990578422e63
daniel <danielddemissie@gmail.com>
2022-05-13 23:31:24 +0300

    finishing api

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 require("dotenv").config();
 const conn = require("./db.config");
 const userRoute = require("./route");
-const route = express.Router();
 const app = express();
 
 app.use(cors());
@@ -11,13 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRoute);
-route.get("/", (req, res) => {
-  res.send({
-    message: "it works",
-  });
-});
-app.use("/", route);
-
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
   conn();
