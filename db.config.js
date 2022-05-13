2c14087248b8971d4994801fbc894a59a7a505b4
daniel <danielddemissie@gmail.com>
2022-05-13 23:41:49 +0300

    finishing api

--- db.config.js
+++ db.config.js
@@ -1,10 +1,10 @@
-const { connect } = require("mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
 module.exports = () => {
-  connect(
+  mongoose.connect(
     `${MONGODB_URL}`,
     {
       useNewUrlParser: true,
