7e11b1ea65360d049c7ffcb73a18fd2a375e92b9
daniel <danielddemissie@gmail.com>
2022-05-13 23:57:44 +0300

    finishing api

--- db.config.js
+++ db.config.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+const MONGODB_URL = process.env.MONGODB_URL;
+
 module.exports = () => {
   mongoose.connect(
     `${MONGODB_URL}`,
