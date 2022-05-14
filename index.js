24686122a4bad17c733f1812e5649500c31ad1e4
daniel <danielddemissie@gmail.com>
2022-05-14 15:38:26 +0300

    update api

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const conn = require("./db.config");
-const userRoute = require("./route");
+const { defaultRoute, userRoute } = require("./route");
 const app = express();
-const router = express.Router();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRoute);
-router.get("/", (req, res) => {
-  res.send({
-    message: "it works",
-  });
-});
+app.use(defaultRoute);
+
 const PORT = process.env.PORT || 4200;
 app.listen(PORT, () => {
   conn();
--- route.js
+++ route.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
 const { addUser, getAllusers, editUser, deleteUser } = require("./controller");
 const route = Router();
+const defaultRoute = route.get("/", (req, res) => {
+  res.send({
+    message: "test addissoftware api",
+  });
+});
 const userRoute = route
   .post("/add", addUser)
   .get("/all", getAllusers)
   .put("/edit/:_id", editUser)
   .delete("/delete/:_id", deleteUser);
-module.exports = userRoute;
+module.exports = {
+  defaultRoute,
+  userRoute,
+};
